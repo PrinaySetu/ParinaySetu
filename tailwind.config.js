3d1ee9f960dff466140e4f3b59c81902007c7c68
Rohit Gupta <93462249+Guptarohit2003@users.noreply.github.com>
2024-05-22 00:49:04 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,46 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
-  theme: {
-    extend: {},
+  "content": [
+    "./src/**/*.{js,jsx,ts,tsx}"
+  ],
+  "theme": {
+    "extend": {
+      "colors": {
+        "white": "#fff",
+        "black": "#000",
+        "red": "#fe0505",
+        "gainsboro": "#e6e6e6",
+        "darkslategray": "#454545",
+        "khaki": {
+          "100": "#ffe27a",
+          "200": "#fede79"
+        },
+        "whitesmoke": "#f7f7f7",
+        "gray": "#828282"
+      },
+      "spacing": {
+        "spacing-xs": "8px",
+        "spacing-s": "24px",
+        "spacing-m": "48px",
+        "spacing-sm": "32px"
+      },
+      "fontFamily": {
+        "body-text": "Inter",
+        "norican": "Norican",
+        "niconne": "Niconne"
+      }
+    },
+    "fontSize": {
+      "45xl": "64px",
+      "37xl": "56px",
+      "5xl": "24px",
+      "base": "16px",
+      "xl": "20px",
+      "21xl": "40px",
+      "inherit": "inherit"
+    }
   },
-  plugins: [],
-};
+  "corePlugins": {
+    "preflight": false
+  }
+}
