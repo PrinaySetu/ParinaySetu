f4e6fb42a48216d4d5dbe723b88379c8728297e7
Rohit Gupta <93462249+Guptarohit2003@users.noreply.github.com>
2024-07-17 18:22:55 +0530

    "Updated styles and layout for Navbar, AdminLoginForm, and AdminSignupForm components"

--- src/components/core/Auth/AdminLoginForm.js
+++ src/components/core/Auth/AdminLoginForm.js
@@ -8,6 +8,7 @@ import HomeIcon from '../../../assets/img/bxs-home.svg'; // Add this if you have
 const AdminLoginForm = ({ setIsLoggedIn }) => {
     const navigate = useNavigate();
     const dispatch = useDispatch();
+    const [isFocused, setIsFocused] = useState(false);
     const [formData, setFormData] = useState({
         email: "",
         password: ""
@@ -28,17 +29,17 @@ const AdminLoginForm = ({ setIsLoggedIn }) => {
     }
 
     return (
-        <div className="min-h-screen flex flex-col justify-center items-center bg-khaki-100">
-            <div className="w-full max-w-md bg-white p-8 rounded-2xl shadow-md relative">
-                <h1 className="text-4xl font-extrabold mb-4">
+        <div className="min-h-screen flex flex-col justify-center items-center bg-khaki-100 px-4">
+            <div className="flex flex-col w-full max-w-60 min-[424px]:max-w-xs sm:max-w-sm bg-white p-6 sm:p-8 rounded-2xl shadow-md">
+                <h1 className="text-3xl sm:text-4xl font-extrabold mb-4 text-center">
                     Login As <span className="text-red">Admin!</span>
                 </h1>
-                <p className="text-gray-500 mb-8">
+                <p className="text-gray-500 mb-8 text-center">
                     To contact us, fill out the form below and we’ll get back to you within 24 hours.
                 </p>
                 <form onSubmit={submitHandler} className="flex flex-col gap-y-4">
-                    <label className="w-full">
-                        <p className="text-[0.875rem] text-gray-900 mb-1 leading-[1.375rem]">
+                    <label className="w-full flex flex-col">
+                        <p className="text-sm sm:text-[0.875rem] text-gray-900 mb-1 leading-[1.375rem]">
                             Email Address<sup className="text-pink-200">*</sup>
                         </p>
                         <input
@@ -48,37 +49,50 @@ const AdminLoginForm = ({ setIsLoggedIn }) => {
                             onChange={changeHandler}
                             placeholder="Enter email id"
                             name="email"
-                            className="bg-white rounded-lg text-gray-900 w-full p-[12px] border-[1px] border-solid border-stroke-little"
+                            className="bg-white rounded-lg text-gray-900 p-3 border-[1px] border-solid border-stroke-little"
                         />
                     </label>
-                    <label className="w-full relative">
-                        <p className="text-[0.875rem] text-gray-900 mb-1 leading-[1.375rem]">
+                    <label className="w-full flex flex-col">
+                        <p className="text-sm sm:text-[0.875rem] text-gray-900 mb-1 leading-[1.375rem]">
                             Password<sup className="text-pink-200">*</sup>
                         </p>
-                        <input
-                            required
-                            type={showPassword ? "text" : "password"}
-                            value={formData.password}
-                            onChange={changeHandler}
-                            placeholder="Enter Password"
-                            name="password"
-                            className="bg-white rounded-lg text-gray-900 w-full p-[12px] border-[1px] border-solid border-stroke-little"
-                        />
-                        <span onClick={() => setShowPassword((prev) => !prev)}
-                            className="absolute right-1 top-[55px] cursor-pointer">
-                            {showPassword ? (<AiOutlineEyeInvisible fontSize={24} fill='#AFB3bf' />) : (<AiOutlineEye fontSize={24} fill='#AFB3bf' />)}
-                        </span>
+                        <div className={`flex justify-between rounded-lg border-[1px] border-solid ${isFocused ? 'border-[2px] border-solid' : 'border-stroke-little'
+                            }`}>
+                            <input
+                                required
+                                type={showPassword ? 'text' : 'password'}
+                                value={formData.password}
+                                onChange={changeHandler}
+                                onFocus={() => setIsFocused(true)}
+                                onBlur={() => setIsFocused(false)}
+                                placeholder="Enter Password"
+                                name="password"
+                                className="bg-white text-gray-900 p-3 w-4/5 outline-none rounded-lg"
+                            />
+                            <span
+                                onClick={() => setShowPassword((prev) => !prev)}
+                                className="flex cursor-pointer items-center px-3"
+                            >
+                                {showPassword ? (
+                                    <AiOutlineEyeInvisible fontSize={24} fill="#AFB3bf" />
+                                ) : (
+                                    <AiOutlineEye fontSize={24} fill="#AFB3bf" />
+                                )}
+                            </span>
+                        </div>
                         <Link to="/forgetpassword">
                             <p className="text-xs mt-1 text-darkblue">Forgot Password</p>
                         </Link>
                     </label>
-                    <button className="w-full bg-red rounded-lg text-white py-2 mt-4 cursor-pointer">SEND</button>
+                    <button
+                        type="submit"
+                        className="w-full bg-red rounded-lg text-white py-2 mt-4 cursor-pointer">Sign In</button>
                 </form>
-                <div className="absolute top-14 right-11">
+                {/* <div className="absolute top-14 right-11">
                     <Link to="/">
                         <img src={HomeIcon} alt="Home" className="w-8 h-8 cursor-pointer" />
                     </Link>
-                </div>
+                </div> */}
             </div>
         </div>
     );
--- src/components/core/Auth/AdminSignupForm.js
+++ src/components/core/Auth/AdminSignupForm.js
@@ -21,6 +21,8 @@ const AdminSignupForm = () => {
 
   const [showPassword, setShowPassword] = useState(false);
   const [showConfirmPassword, setShowConfirmPassword] = useState(false);
+  const [isFocused, setIsFocused] = useState(false);
+  const [isFocused1, setIsFocused1] = useState(false);
 
   const { firstName, lastName, email, password, confirmPassword } = formData;
 
@@ -59,77 +61,100 @@ const AdminSignupForm = () => {
   };
 
   return (
-    <div className="min-h-screen flex flex-col py-[100px] justify-center items-center bg-khaki-100">
-      <div className="w-full max-w-md bg-white p-8 rounded-2xl shadow-md relative">
-        <h1 className="text-4xl font-extrabold mb-4">
+    <div className="min-h-screen flex flex-col py-24 justify-center items-center bg-khaki-100">
+      <div className="flex flex-col w-full max-w-60 min-[424px]:max-w-xs sm:max-w-sm bg-white p-6 sm:p-8 rounded-2xl shadow-md">
+        <h1 className="text-3xl sm:text-4xl font-extrabold mb-4 text-center">
           Sign Up <span className="text-red">as Admin!</span>
         </h1>
-        <p className="text-gray-500 mb-8">
+        <p className="text-gray-500 mb-8 text-center">
           To join us, fill out the form below and we’ll get back to you within 24 hours.
         </p>
-        <form onSubmit={handleOnSubmit} className="flex flex-col gap-y-4 text-black">
-          <label>
-            <p className="mb-1 text-[0.875rem] leading-[1.375rem] text-gray-900">First Name</p>
+        <form onSubmit={handleOnSubmit} className="flex flex-col gap-y-4">
+          <label className="w-full flex flex-col">
+            <p className="mb-1 text-sm sm:text-[0.875rem] leading-[1.375rem] text-gray-900">First Name<sup className="text-pink-200">*</sup></p>
             <input
               type="text"
               name="firstName"
               value={firstName}
               onChange={handleOnChange}
               placeholder="First Name"
-              className="bg-white rounded-lg text-gray-900 w-full p-[12px] border-[1px] border-solid border-stroke-little"
+              className="bg-white rounded-lg text-gray-900 p-3 border-[1px] border-solid border-stroke-little"
+              required
             />
           </label>
-          <label>
-            <p className="mb-1 text-[0.875rem] leading-[1.375rem] text-gray-900">Last Name</p>
+          <label className="w-full flex flex-col">
+            <p className="mb-1 text-sm sm:text-[0.875rem] leading-[1.375rem] text-gray-900">Last Name<sup className="text-pink-200">*</sup></p>
             <input
               type="text"
               name="lastName"
               value={lastName}
               onChange={handleOnChange}
               placeholder="Last Name"
-              className="bg-white rounded-lg text-gray-900 w-full p-[12px] border-[1px] border-solid border-stroke-little"
+              className="bg-white rounded-lg text-gray-900 p-3 border-[1px] border-solid border-stroke-little"
+              required
             />
           </label>
-          <label>
-            <p className="mb-1 text-[0.875rem] leading-[1.375rem] text-gray-900">Email</p>
+          <label className="w-full flex flex-col">
+            <p className="text-sm sm:text-[0.875rem] text-gray-900 mb-1 leading-[1.375rem]">
+              Email<sup className="text-pink-200">*</sup>
+            </p>
             <input
               type="email"
               name="email"
               value={email}
               onChange={handleOnChange}
               placeholder="Email"
-              className="bg-white rounded-lg text-gray-900 w-full p-[12px] border-[1px] border-solid border-stroke-little"
+              className="bg-white rounded-lg text-gray-900 p-3 border-[1px] border-solid border-stroke-little"
+              required
             />
           </label>
-          <label className="relative">
-            <p className="mb-1 text-[0.875rem] leading-[1.375rem] text-gray-900">Password</p>
-            <input
-              type={showPassword ? 'text' : 'password'}
-              name="password"
-              value={password}
-              onChange={handleOnChange}
-              placeholder="Password"
-              className="bg-white rounded-lg text-gray-900 w-full p-[12px] border-[1px] border-solid border-stroke-little"
-            />
-            <span onClick={() => setShowPassword(!showPassword)} className="absolute right-1 top-[50px] cursor-pointer">
-              {showPassword ? <AiOutlineEyeInvisible fontSize={24} fill='#AFB3bf' /> : <AiOutlineEye fontSize={24} fill='#AFB3bf' />}
-            </span>
+          <label className="w-full relative">
+            <p className="text-[0.875rem] text-gray-900 mb-1 leading-[1.375rem]">
+              Password<sup className="text-pink-200">*</sup>
+            </p>
+            <div className={`flex justify-between rounded-lg border-[1px] border-solid ${isFocused1 ? 'border-[2px] border-solid' : 'border-stroke-little'
+              }`}>
+              <input
+                type={showPassword ? 'text' : 'password'}
+                name="password"
+                value={password}
+                onChange={handleOnChange}
+                placeholder="Password"
+                onFocus={() => setIsFocused1(true)}
+                onBlur={() => setIsFocused1(false)}
+                className="bg-white rounded-lg text-gray-900 p-3 w-4/5 outline-none"
+                required
+              />
+              <span onClick={() => setShowPassword(!showPassword)}
+                className="flex items-center px-3 cursor-pointer">
+                {showPassword ? (<AiOutlineEyeInvisible fontSize={24} fill='#AFB3bf' />) : (<AiOutlineEye fontSize={24} fill='#AFB3bf' />)}
+              </span>
+            </div>
           </label>
-          <label className="relative">
-            <p className="mb-1 text-[0.875rem] leading-[1.375rem] text-gray-900">Confirm Password</p>
-            <input
-              type={showConfirmPassword ? 'text' : 'password'}
-              name="confirmPassword"
-              value={confirmPassword}
-              onChange={handleOnChange}
-              placeholder="Confirm Password"
-              className="bg-white rounded-lg text-gray-900 w-full p-[12px] border-[1px] border-solid border-stroke-little"
-            />
-            <span onClick={() => setShowConfirmPassword(!showConfirmPassword)} className="absolute right-1 top-[50px] cursor-pointer">
-              {showConfirmPassword ? <AiOutlineEyeInvisible fontSize={24} fill='#AFB3bf' /> : <AiOutlineEye fontSize={24} fill='#AFB3bf' />}
-            </span>
+          <label className="w-full flex flex-col">
+            <p className="text-sm sm:text-[0.875rem] text-gray-900 mb-1 leading-[1.375rem]">
+              Confirm Password<sup className="text-pink-200">*</sup>
+            </p>
+            <div className={`flex justify-between rounded-lg border-[1px] border-solid ${isFocused ? 'border-[2px] border-solid' : 'border-stroke-little'
+              }`}>
+              <input
+                type={showConfirmPassword ? 'text' : 'password'}
+                name="confirmPassword"
+                value={confirmPassword}
+                onChange={handleOnChange}
+                placeholder="Confirm Password"
+                onFocus={() => setIsFocused(true)}
+                onBlur={() => setIsFocused(false)}
+                className="bg-white rounded-lg text-gray-900 w-4/5 p-3 outline-none"
+                required
+              />
+              <span onClick={() => setShowConfirmPassword(!showConfirmPassword)}
+                className="flex items-center px-3 cursor-pointer">
+                {showConfirmPassword ? (<AiOutlineEyeInvisible fontSize={24} fill='#AFB3bf' />) : (<AiOutlineEye fontSize={24} fill='#AFB3bf' />)}
+              </span>
+            </div>
           </label>
-          <button type="submit" className="w-full bg-red rounded-lg text-white py-2 mt-4">Create Account</button>
+          <button type="submit" className="w-full bg-red rounded-lg text-white py-2 mt-4 cursor-pointer">Create Account</button>
         </form>
       </div>
     </div>
