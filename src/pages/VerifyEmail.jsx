9bb3f10f4cfb8639b52ebe07fbcc828f0e373ce8
Rohit Gupta <93462249+Guptarohit2003@users.noreply.github.com>
2024-07-18 16:08:22 +0530

    "Refactor VerifyEmail component: updated layout, styling, and OTP input"

--- src/pages/VerifyEmail.jsx
+++ src/pages/VerifyEmail.jsx
@@ -31,39 +31,42 @@ const VerifyEmail = () => {
   };
 
   return (
-    <div className="min-h-screen grid place-items-center bg-khaki-100">
+    <div className="flex flex-col min-h-screen place-items-center bg-khaki-100 px-4 py-24">
       {loading ? (
         <div>
           <div className="spinner"></div>
         </div>
       ) : (
-        <div className="w-full max-w-md bg-white p-8 rounded-2xl shadow-md relative">
-          <h1 className="text-4xl font-extrabold mb-4 text-center">
-            Verify <span className="text-red-500">Email</span>
+        <div className="flex flex-col w-full max-w-[200px] sm:max-w-xs md:max-w-sm lg:max-w-md bg-white p-8 rounded-2xl justify-center text-center shadow-md">
+          <h1 className="text-3xl sm:text-4xl font-extrabold mb-4 text-center">
+            Verify <span className="text-red">Email</span>
           </h1>
           <p className="text-gray-500 mb-8 text-center">
             A verification code has been sent to you. Enter the code below.
           </p>
-          <form onSubmit={handleVerifyAndSignup}>
-            <OtpInput
-              value={otp}
-              onChange={setOtp}
-              numInputs={6}
-              renderInput={(props) => (
-                <input
-                  {...props}
-                  placeholder="-"
-                  style={{
-                    boxShadow: 'inset 0px -1px 0px rgba(255, 255, 255, 0.18)',
-                  }}
-                  className="w-[48px] lg:w-[60px] border-0 bg-gray-100 rounded-lg text-gray-900 border-[1px] border-solid border-stroke-little aspect-square text-center focus:border-0 focus:outline-2 focus:outline-yellow-500"
-                />
-              )}
-              containerStyle={{
-                justifyContent: 'space-between',
-                gap: '0 6px',
-              }}
-            />
+          <form onSubmit={handleVerifyAndSignup} className="flex flex-col">
+            <div className="px-8 sm:p-0">
+              <OtpInput
+                value={otp}
+                onChange={setOtp}
+                numInputs={6}
+                renderInput={(props) => (
+                  <input
+                    {...props}
+                    placeholder="-"
+                    style={{
+                      boxShadow: 'inset 0px -1px 0px rgba(255, 255, 255, 0.18)',
+                    }}
+                    className="flex w-[35px] md:w-[48px] lg:w-[60px] border-0 rounded-lg text-gray-900 border-[1px] border-solid border-stroke-little aspect-square text-center focus:border-0 focus:outline-2 focus:outline-yellow-500"
+                  />
+                )}
+                containerStyle={{
+                  justifyContent: 'space-between',
+                  gap: '10px 6px',
+                  flexWrap: 'wrap',
+                }}
+              />
+            </div>
             <button
               type="submit"
               className="w-full bg-red py-3 px-6 rounded-lg mt-6 font-medium text-white"
@@ -71,7 +74,7 @@ const VerifyEmail = () => {
               Verify Email
             </button>
           </form>
-          <div className="mt-6 flex items-center justify-between">
+          <div className="mt-6 flex flex-col items-center justify-between sm:flex-row">
             <Link to="/signup" style={{ color: 'inherit', textDecoration: 'none' }}>
               <p className="text-gray-900 flex items-center gap-x-2">
                 <BiArrowBack /> Back To Signup
