e2f0c7977f7f90d95efd58f041a02faacdae1326
Rohit Gupta <93462249+Guptarohit2003@users.noreply.github.com>
2024-07-25 14:53:32 +0530

    "Refactored code in Auth components, including SignupForm, UpdatePassword, and ForgetPassword, with changes to layout, styling, and minor logic updates."

--- src/components/core/Auth/UpdatePassword.js
+++ src/components/core/Auth/UpdatePassword.js
@@ -1,42 +1,42 @@
-import React, { useState } from "react"
-import { useForm } from "react-hook-form"
-import { AiOutlineEye, AiOutlineEyeInvisible } from "react-icons/ai"
-import { useSelector } from "react-redux"
-import { useNavigate } from "react-router-dom"
+import React, { useState } from "react";
+import { useForm } from "react-hook-form";
+import { AiOutlineEye, AiOutlineEyeInvisible } from "react-icons/ai";
+import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
 
-import { changePassword } from "../../../services/operations/authAPi"
-import IconBtn from "../../../pages/IconBtn"
+import { changePassword } from "../../../services/operations/authAPi";
+import IconBtn from "../../../pages/IconBtn";
 
 export default function ChangePassword() {
-  const { token } = useSelector((state) => state.auth)
-  const navigate = useNavigate()
+  const { token } = useSelector((state) => state.auth);
+  const navigate = useNavigate();
 
-  const [showOldPassword, setShowOldPassword] = useState(false)
-  const [showNewPassword, setShowNewPassword] = useState(false)
+  const [showOldPassword, setShowOldPassword] = useState(false);
+  const [showNewPassword, setShowNewPassword] = useState(false);
 
   const {
     register,
     handleSubmit,
     formState: { errors },
-  } = useForm()
+  } = useForm();
 
   const submitPasswordForm = async (data) => {
     // console.log("password Data - ", data)
     try {
-      await changePassword(token, data)
+      await changePassword(token, data);
     } catch (error) {
-      console.log("ERROR MESSAGE - ", error.message)
+      console.log("ERROR MESSAGE - ", error.message);
     }
-  }
+  };
 
   return (
-    <>
-      <form onSubmit={handleSubmit(submitPasswordForm)}>
-        <div className="my-10 flex flex-col gap-y-6 rounded-md border-[1px] border-richblack-700 bg-richblack-800 p-8 px-12">
-          <h2 className="text-lg font-semibold text-richblack-5">Password</h2>
-          <div className="flex flex-col gap-5 lg:flex-row">
-            <div className="relative flex flex-col gap-2 lg:w-[48%]">
-              <label htmlFor="oldPassword" className="lable-style">
+    <div className="min-h-screen flex flex-col justify-center items-center bg-cornsilk px-4">
+      <div className="flex flex-col w-full max-w-60 min-[424px]:max-w-xs sm:max-w-sm lg:max-w-lg bg-white p-6 sm:p-8 rounded-2xl shadow-md">
+        <h2 className="text-3xl font-extrabold mb-4 text-richblack-5">Change <span className="text-red">Password</span></h2>
+        <form onSubmit={handleSubmit(submitPasswordForm)} className="flex flex-col gap-y-4">
+          <div className="flex flex-col gap-6">
+            <div className="relative flex flex-col gap-2">
+              <label htmlFor="oldPassword" className="text-sm sm:text-[0.875rem] text-gray-900 leading-[1.375rem]">
                 Current Password
               </label>
               <input
@@ -44,27 +44,27 @@ export default function ChangePassword() {
                 name="oldPassword"
                 id="oldPassword"
                 placeholder="Enter Current Password"
-                className="form-style"
+                className="bg-white rounded-lg text-gray-900 p-3 border-[1px] border-solid border-stroke-little"
                 {...register("oldPassword", { required: true })}
               />
               <span
                 onClick={() => setShowOldPassword((prev) => !prev)}
-                className="absolute right-3 top-[38px] z-[10] cursor-pointer"
+                className="absolute right-3 top-[38px] cursor-pointer"
               >
                 {showOldPassword ? (
-                  <AiOutlineEyeInvisible fontSize={24} fill="#AFB2BF" />
+                  <AiOutlineEyeInvisible fontSize={24} fill="#AFB3bf" />
                 ) : (
-                  <AiOutlineEye fontSize={24} fill="#AFB2BF" />
+                  <AiOutlineEye fontSize={24} fill="#AFB3bf" />
                 )}
               </span>
               {errors.oldPassword && (
-                <span className="-mt-1 text-[12px] text-yellow-100">
+                <span className="-mt-1 text-[12px] text-pink-200">
                   Please enter your Current Password.
                 </span>
               )}
             </div>
-            <div className="relative flex flex-col gap-2 lg:w-[48%]">
-              <label htmlFor="newPassword" className="lable-style">
+            <div className="relative flex flex-col gap-2">
+              <label htmlFor="newPassword" className="text-sm sm:text-[0.875rem] text-gray-900 leading-[1.375rem]">
                 New Password
               </label>
               <input
@@ -72,39 +72,37 @@ export default function ChangePassword() {
                 name="newPassword"
                 id="newPassword"
                 placeholder="Enter New Password"
-                className="form-style"
+                className="bg-white rounded-lg text-gray-900 p-3 border-[1px] border-solid border-stroke-little"
                 {...register("newPassword", { required: true })}
               />
               <span
                 onClick={() => setShowNewPassword((prev) => !prev)}
-                className="absolute right-3 top-[38px] z-[10] cursor-pointer"
+                className="absolute right-3 top-[38px] cursor-pointer"
               >
                 {showNewPassword ? (
-                  <AiOutlineEyeInvisible fontSize={24} fill="#AFB2BF" />
+                  <AiOutlineEyeInvisible fontSize={24} fill="#AFB3bf" />
                 ) : (
-                  <AiOutlineEye fontSize={24} fill="#AFB2BF" />
+                  <AiOutlineEye fontSize={24} fill="#AFB3bf" />
                 )}
               </span>
               {errors.newPassword && (
-                <span className="-mt-1 text-[12px] text-yellow-100">
+                <span className="-mt-1 text-[12px] text-pink-200">
                   Please enter your New Password.
                 </span>
               )}
             </div>
           </div>
-        </div>
-        <div className="flex justify-end gap-2">
-          <button
-            onClick={() => {
-              navigate("/my-dashboard")
-            }}
-            className="cursor-pointer rounded-md bg-richblack-700 py-2 px-5 font-semibold text-richblack-50"
-          >
-            Cancel
-          </button>
-          <IconBtn type="submit" text="Update" />
-        </div>
-      </form>
-    </>
-  )
+          <div className="flex justify-end gap-2 mt-6">
+            <button
+              onClick={() => navigate("/my-dashboard")}
+              className="cursor-pointer rounded-lg bg-gray-700 py-2 px-5 font-semibold text-white"
+            >
+              Cancel
+            </button>
+            <IconBtn type="submit" text="Update" />
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 }
--- src/pages/UpdatePassword.jsx
+++ src/pages/UpdatePassword.jsx
@@ -1,120 +1,128 @@
-import React, { useState } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
+import React, { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { resetPassword } from '../services/operations/authAPi';
 import { Link, useLocation, useNavigate } from 'react-router-dom';
-
-import { AiOutlineEye, AiOutlineEyeInvisible } from "react-icons/ai"
-import { BiArrowBack } from "react-icons/bi"
+import { AiOutlineEye, AiOutlineEyeInvisible } from "react-icons/ai";
+import { BiArrowBack } from "react-icons/bi";
 
 const UpdatePassword = () => {
-    const location = useLocation();
-    const navigate = useNavigate();
-    const {loading} = useSelector((state)=> state.auth)
-    const dispatch = useDispatch();
-    const[formData , setFormData] = useState({
-        password:"",
-        confirmPassword:""
-    })
-    const [showPassword, setShowPassword] = useState(false)
-    const [showConfirmPassword, setShowConfirmPassword] = useState(false)
-    const { password, confirmPassword } = formData
-    const handleOnChange= (e)=>{
-        setFormData((prevData)=>(
-            {
-                ...prevData,
-                [e.target.name] : e.target.value,
-            }
-        ))
-    }
-    const handleOnSubmit = (e)=>{
-        e.preventDefault();
-        // console.log(formData);
-        const token = location.pathname.split('/').at(-1);
-        dispatch(resetPassword(password , confirmPassword , token));
-    }
-  return (
-    <div className="grid min-h-[calc(100vh-3.5rem)] place-items-center">
-    {loading ? (
-      <div className="spinner"></div>
-    ) : (
-      <div className="max-w-[500px] p-4 lg:p-8">
-        <h1 className="text-[1.875rem] font-semibold leading-[2.375rem] text-richblack-5">
-          Choose new password
-        </h1>
-        <p className="my-4 text-[1.125rem] leading-[1.625rem] text-richblack-100">
-          Almost done. Enter your new password and youre all set.
-        </p>
-                <form onSubmit={handleOnSubmit}>
+  const location = useLocation();
+  const navigate = useNavigate();
+  const { loading } = useSelector((state) => state.auth);
+  const dispatch = useDispatch();
+  const [formData, setFormData] = useState({
+    password: "",
+    confirmPassword: ""
+  });
+  const [showPassword, setShowPassword] = useState(false);
+  const [showConfirmPassword, setShowConfirmPassword] = useState(false);
+  const { password, confirmPassword } = formData;
+  const [isFocused, setIsFocused] = useState(false);
+  const [isFocused1, setIsFocused1] = useState(false);
 
-                <label className="relative">
-              <p className="mb-1 text-[0.875rem] leading-[1.375rem] text-richblack-5">
+  const handleOnChange = (e) => {
+    setFormData((prevData) => ({
+      ...prevData,
+      [e.target.name]: e.target.value,
+    }));
+  };
+
+  const handleOnSubmit = (e) => {
+    e.preventDefault();
+    const token = location.pathname.split('/').at(-1);
+    dispatch(resetPassword(password, confirmPassword, token));
+  };
+
+  return (
+    <div className="min-h-screen flex flex-col justify-center items-center bg-cornsilk px-4">
+      {loading ? (
+        <div className="spinner"></div>
+      ) : (
+        <div className="flex flex-col w-full max-w-60 min-[424px]:max-w-xs sm:max-w-sm lg:max-w-lg bg-white p-6 sm:p-8 rounded-2xl shadow-md">
+          <h1 className="text-3xl font-extrabold mb-4 text-richblack-5">
+            Choose new password
+          </h1>
+          <p className="my-4 text-[1.125rem] leading-[1.625rem] text-gray-500">
+            Almost done. Enter your new password and you're all set.
+          </p>
+          <form onSubmit={handleOnSubmit} className="flex flex-col gap-y-4">
+            <label className="flex flex-col w-full">
+              <p className="text-sm sm:text-[0.875rem] text-gray-900 mb-1 leading-[1.375rem]">
                 New Password <sup className="text-pink-200">*</sup>
               </p>
+              <div className={`flex justify-between rounded-lg border-[1px] border-solid ${isFocused1 ? 'border-[2px] border-solid' : 'border-stroke-little'
+                }`}>
+                <input
+                  required
+                  type={showPassword ? "text" : "password"}
+                  value={password}
+                  name="password"
+                  onChange={handleOnChange}
+                  placeholder="Enter Password"
+                  onFocus={() => setIsFocused1(true)}
+                  onBlur={() => setIsFocused1(false)}
+                  className="bg-white rounded-lg text-gray-900 p-3 w-4/5 outline-none"
+                />
+                <span
+                  onClick={() => setShowPassword((prev) => !prev)}
+                  className="flex cursor-pointer items-center px-3"
+                >
+                  {showPassword ? (
+                    <AiOutlineEyeInvisible fontSize={24} fill="#AFB2BF" />
+                  ) : (
+                    <AiOutlineEye fontSize={24} fill="#AFB2BF" />
+                  )}
+                </span>
+              </div>
 
-                        <input
-
-                            required
-                            type={showPassword ? "text" :"password"}
-                            value={password}
-                            name='password'
-                            onChange={handleOnChange}
-                            placeholder="Enter Password"
-                            className="form-style w-full !pr-10"
-                        />
-                        <span
-                onClick={() => setShowPassword((prev) => !prev)}
-                className="absolute right-3 top-[38px] z-[10] cursor-pointer"
-              >
-                {showPassword ? (
-                  <AiOutlineEyeInvisible fontSize={24} fill="#AFB2BF" />
-                ) : (
-                  <AiOutlineEye fontSize={24} fill="#AFB2BF" />
-                )}
-              </span>
-                    </label>
-                    <label className="relative mt-3 block">
-              <p className="mb-1 text-[0.875rem] leading-[1.375rem] text-richblack-5">
+            </label>
+            <label className="w-full flex flex-col">
+              <p className="text-sm sm:text-[0.875rem] text-gray-900 mb-1 leading-[1.375rem]">
                 Confirm New Password <sup className="text-pink-200">*</sup>
               </p>
-              <input
-                required
-                type={showConfirmPassword ? "text" : "password"}
-                name="confirmPassword"
-                value={confirmPassword}
-                onChange={handleOnChange}
-                placeholder="Confirm Password"
-                className="form-style w-full !pr-10"
-              />
-              <span
-                onClick={() => setShowConfirmPassword((prev) => !prev)}
-                className="absolute right-3 top-[38px] z-[10] cursor-pointer"
-              >
-                {showConfirmPassword ? (
-                  <AiOutlineEyeInvisible fontSize={24} fill="#AFB2BF" />
-                ) : (
-                  <AiOutlineEye fontSize={24} fill="#AFB2BF" />
-                )}
-              </span>
+              <div className={`flex justify-between rounded-lg border-[1px] border-solid ${isFocused ? 'border-[2px] border-solid' : 'border-stroke-little'
+                }`}>
+                <input
+                  required
+                  type={showConfirmPassword ? "text" : "password"}
+                  name="confirmPassword"
+                  value={confirmPassword}
+                  onChange={handleOnChange}
+                  placeholder="Confirm Password"
+                  onFocus={() => setIsFocused(true)}
+                  onBlur={() => setIsFocused(false)}
+                  className="bg-white rounded-lg text-gray-900 w-4/5 p-3 outline-none"
+                />
+                <span
+                  onClick={() => setShowConfirmPassword((prev) => !prev)}
+                  className="flex items-center px-3 cursor-pointer"
+                >
+                  {showConfirmPassword ? (
+                    <AiOutlineEyeInvisible fontSize={24} fill="#AFB2BF" />
+                  ) : (
+                    <AiOutlineEye fontSize={24} fill="#AFB2BF" />
+                  )}
+                </span>
+              </div>
             </label>
             <button
               type="submit"
-              className="mt-6 w-full rounded-[8px] bg-yellow-50 py-[12px] px-[12px] font-medium text-richblack-900"
+              className="mt-6 w-full rounded-lg bg-khaki-100 py-2 font-medium text-white"
             >
               Reset Password
             </button>
-                </form>
-                <div className="mt-6 flex items-center justify-between">
+          </form>
+          <div className="mt-6 flex items-center justify-between">
             <Link to="/login">
-              <p className="flex items-center gap-x-2 text-richblack-5">
+              <p className="flex items-center gap-x-2 text-darkblue">
                 <BiArrowBack /> Back To Login
               </p>
             </Link>
           </div>
-            </div>
-        )
-      }
+        </div>
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default UpdatePassword
+export default UpdatePassword;
