c32187a7fb847553579b5332a5650613572a6909
Piyush12800 <12800piyush@gmail.com>
2024-10-28 16:42:53 +0530

    Legal

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -169,7 +169,7 @@ const Home = () => {
               <Link to="/terms-and-conditions" style={{ textDecoration: 'none' }} className="leading-[150%] font-medium text-darkslategray hover:text-red">Terms and Conditions</Link>
               <Link to="/privacy-policy" style={{ textDecoration: 'none' }} className="leading-[150%] font-medium text-darkslategray hover:text-red">Privacy Policy</Link>
               <Link to="/refund-policy" style={{ textDecoration: 'none' }} className="leading-[150%] font-medium text-darkslategray hover:text-red">Refund Policy</Link>
-              <Link to="/shipping-and-delivery" style={{ textDecoration: 'none' }} className="leading-[150%] font-medium text-darkslategray hover:text-red">Shopping and Delievery Policy</Link>
+              <Link to="/shipping-and-delivery" style={{ textDecoration: 'none' }} className="leading-[150%] font-medium text-darkslategray hover:text-red">Shipping and Delievery Policy</Link>
 
             </div>
             <div className="flex flex-col items-start lg:items-center gap-4">
