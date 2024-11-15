c48a7d77ff6f5682b3cac75e2f615a7850930b99
PrinaySetu <parinaysetu2701@gmail.com>
2024-11-15 09:58:22 +0530

    Update Footer.js

--- src/components/Common/Footer.js
+++ src/components/Common/Footer.js
@@ -6,7 +6,7 @@ const Group = () => {
             <div className="flex justify-center items-center gap-4">
                 <img src={logo} alt="Logo" className="w-12 h-12" />
                 <div className="flex flex-col justify-center items-center font-niconne">
-                    <p className="text-gray-800 text-5xl sm:text-13xl m-0">Parinay</p>
+                    <p className="text-black text-5xl sm:text-13xl m-0">Parinay</p>
                     <p className="self-end text-5xl sm:text-13xl text-red m-0 mt-[-5px] mr-[-20px]">Setu</p>
                 </div>
             </div>
